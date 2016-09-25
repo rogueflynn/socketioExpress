c4f0b6e0cb48492d918907edb96dfcfcfc640d5a
rogueflynn <rogueflynn@debian>
2016-09-25 15:06:32 -0400

    Added parse query in the users route file and passed the results from the query to the socket.io emitter.

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -4,8 +4,20 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	var test = "sup";
-	var arr = ["John", "Victor", "Emily"]; 
-	res.io.emit("socketToMe", arr);
+	var arr = []; 
+	var GameScore = Parse.Object.extend('GameScore');
+	var query = new Parse.Query(GameScore);
+	query.find({
+	success: function(results) {
+		for(var i = 0; i < results.length; i++) {
+			arr.push(results[i].get('playerName'));
+		}
+		res.io.emit("socketToMe", arr);
+	}, 
+	error: function(error) {
+		res.redirect('/users');
+	}
+	});
   	res.send('respond with a resource');
 });
 
